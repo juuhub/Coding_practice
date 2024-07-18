5452079ef2f52e0d83d3894b383551eec018b23c
Juhan Kim <juhankim@handong.ac.kr>
2024-07-18 14:14:43 +0900

    Upload prob 25083

---
+++ C++/inputoutput/BJ25083.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+/*
+큰따옴표(") 출력 -> \"
+작은따옴표(') 출력 -> \'
+역슬래쉬(\) 출력 -> \\
+백틱(`)
+*/
+
+int main(){
+    cout << "         ,r\'\"7" << endl;
+    cout << "r`-_   ,\'  ,/" << endl;
+    cout << " \\. \". L_r\'" << endl;
+    cout << "   `~\\/" << endl;
+    cout << "      |" << endl;
+    cout << "      |" << endl;
+    return 0;
+}
