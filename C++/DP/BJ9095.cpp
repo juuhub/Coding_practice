b517ea4c5c2635d75388f7ed2317eca1bd09939f
juuhub <juhankim@handong.ac.kr>
2025-06-02 13:54:27 +0900

    [Modify] add annotation

--- C++/DP/BJ9095.cpp
+++ C++/DP/BJ9095.cpp
@@ -19,3 +19,18 @@ int main(){
     printf("%d\n", adder[input-1]);
   }
 }
+
+/*
+<Bottom-up>
+adder[0] -> 1
+adder[1] -> 2
+adder[2] -> 4
+adder[3] -> adder[0] + adder[1] + adder[2]
+adder[n] -> adder[n-3] + adder[n-2] + adder[n-1] 
+
+1,2,3만을 이용하여 계산 가능
+So, (N-3) + 3 = N-3 만드는 경우의 수
+
+DP 알고리즘 정의 방법
+1. 최적 구조 파악 -> 2. 문제의 재귀적 정의 -> 3. bottom-up 방법으로 solution 구함
+*/
